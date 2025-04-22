cea0f3c39d8aed9b600dac2f7c779679757de0fe
davidpxello <davidp@xello.world>
2025-04-22 19:28:58 -0400

    Add files via upload

---
+++ sales-scorekeeper-final2/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Scorekeeper from './Scorekeeper'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Scorekeeper />
+  </React.StrictMode>
+)
